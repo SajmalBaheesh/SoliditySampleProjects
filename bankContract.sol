94b22b5147568f8d9459182381b0914975493067
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-06-28 23:34:19 +0530

    Update bankContract.sol

--- bankContract.sol
+++ bankContract.sol
@@ -1,1 +1,33 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+//every customer will be represented by address.
+
+
+contract Bank{
+    mapping(address => uint) ledgerBalance;
+    address public owner; 
+    
+    //Use a constructor - Triggered at the time of deployment
+    constructor() {
+        //Whoever deploys the contract will be the owner
+        owner = msg.sender;
+    }
+
+    //deposit
+    function deposit() public payable {
+        ledgerBalance[msg.sender] = ledgerBalance[msg.sender] + msg.value;
+    }
+
+    //withdraw
+    function withdraw(uint amount) public  {
+       require(ledgerBalance[msg.sender] > amount,"Not enough balance");
+       ledgerBalance[msg.sender] = ledgerBalance[msg.sender] - (amount );
+       payable (msg.sender).transfer(amount);
+
+    }
+    function mybalance() public view returns (uint){
+        return ledgerBalance[msg.sender];
+    }
+}
