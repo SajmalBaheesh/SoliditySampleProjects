daba5170cb86c9e45e7f29de9cbea3d646412f7c
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-07-01 16:27:38 +0530

    Update BookStore.sol: Token.sol as sep instance
    Deploy Toke.sol first
    copy its address
    Give this address while deploying Boostore.sol

--- TokenExample/BookStore.sol
+++ TokenExample/BookStore.sol
@@ -2,8 +2,6 @@
 pragma solidity 0.8.19;
 import "Token.sol";
 
-//inherit below contract from Token
-
 //Bookstore
 contract BookStore is Token {
     struct Book {
@@ -12,13 +10,20 @@ contract BookStore is Token {
         bool sold;
     }
 
+    //create an instance of the contract 
+    Token tObj;
+    //Assume that Token.sol is already deployed
+    constructor(address conAddress){
+        tObj = Token(conAddress);
+    }
+
     uint[]  booklist;
 
     mapping(uint => Book) books;
 
     // fun setting book details
     function setBookDetails(string memory _title, uint8 _price) public returns (uint)  {
-        uint _slno = createToken();
+        uint _slno = tObj.createToken();
         books[_slno] = Book(_title, _price, false);
         booklist.push(_slno);
         return (_slno);
@@ -27,12 +32,12 @@ contract BookStore is Token {
     // buys - ownership tranfer
     // buyer provider ether > price
     function buyBook(uint _slno) public payable {
-        require(tokenOwner[_slno] != msg.sender, 'Already owns it');
+        require(tObj.getOwner(_slno) != msg.sender, 'Already owns it');
 
         if ((msg.value / 1 ether) == books[_slno].price) {
-            changeTokenOwner(_slno,msg.sender);
+            tObj.changeTokenOwner(_slno,msg.sender);
         } else if ((msg.value / 1 ether) > books[_slno].price) {
-            changeTokenOwner(_slno,msg.sender);
+            tObj.changeTokenOwner(_slno,msg.sender);
             uint256 rm = msg.value - books[_slno].price * 1 ether;
 
             payable(msg.sender).transfer(rm);
@@ -46,6 +51,6 @@ contract BookStore is Token {
     }
 
     function getBookDetails(uint _slno) public view returns (Book memory,address) {
-        return (books[_slno],tokenOwner[_slno]);
+        return (books[_slno],tObj.getOwner(_slno));
     }
 }
