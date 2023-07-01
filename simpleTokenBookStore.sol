d33c23c6040e18d104a32f82283b6f1e4953b536
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-07-01 15:28:41 +0530

    Update simpleTokenBookStore.sol

--- simpleTokenBookStore.sol
+++ simpleTokenBookStore.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-//every customer will be represented by address.
-
-
 
 contract Token{
     //Token Owner
@@ -24,75 +21,61 @@ contract Token{
 
 
 
-
-
-contract Bank{
-    mapping(address => uint) ledgerBalance;
-    mapping (uint=>address) addressIndex;
-    address public owner; 
-    uint64 public addrCount;
-    
-    //Use a constructor - Triggered at the time of deployment
-    constructor() {
-        //Whoever deploys the contract will be the owner
-        owner = msg.sender;
-        
+contract BookStore {
+    struct Book {
+        string title;
+        uint8 price;
+        address owner;
+        bool sold;
+       
     }
 
-   //custom modifier, Functions with this modifier should pass the require condition 
-    modifier onlyOwner() {
-        
-        require(msg.sender == owner,"Access Denied");
-        _;
+    //Using a dynamic array for storing book lists 
+    uint[]  booklist;
+
+
+
+    mapping (uint => Book) Books;
+
+
+    // fun setting book details
+    // set title price owner
+    function setBookDetails(uint _slno,string memory _title, uint8 _price) public {
+        Books[_slno] = Book(_title, _price, msg.sender,false);
+        booklist.push(_slno);
     }
 
-    //events
-    event Depositevent(address depositor ,uint amount) ;
+    //Fn overloading: for registering an already sold book
+    function setBookDetails(uint _slno,string memory _title, uint8 _price, bool _soldornot) public {
+        Books[_slno] = Book(_title, _price, msg.sender,_soldornot);
+        booklist.push(_slno);
+    }
 
-    //deposit
-    function deposit() public payable {
-        if(ledgerBalance[msg.sender] ==0){
-            addressIndex[addrCount++] == msg.sender;
+    // buys - ownership transfer
+    // buyer provider ether > price
+    function buyBook(uint _slno) public payable {
+        require( msg.sender != Books[_slno].owner,"You are already the owner");
+        require(Books[_slno].sold,"Book is already sold");
+        if ((msg.value / 1 ether) == Books[_slno].price) {
+            Books[_slno].owner = msg.sender;
+            Books[_slno].sold = true;
+        } else if ((msg.value / 1 ether) > Books[_slno].price) {
+            Books[_slno].owner = msg.sender;
+            uint256 rm = msg.value - Books[_slno].price * 1 ether;
+            Books[_slno].sold = true;
+            payable(msg.sender).transfer(rm);
+            
+        } else {
+            revert();
         }
-        ledgerBalance[msg.sender] = ledgerBalance[msg.sender] + msg.value;
-        emit Depositevent(msg.sender,msg.value);
     }
 
-    //functions to be called by only Owner ,Fns executed only if it passes 
-    function getBankBalance() public view onlyOwner returns(uint){
-        return address(this).balance;
-    } 
-
-    //Send his balance back
-    function deleteAccount()public  {
-        payable (msg.sender).transfer(ledgerBalance[msg.sender]);
-        delete ledgerBalance[msg.sender]; //reset values to default
+    function getBookDetails(uint _slno) public view returns (Book memory) {
+        return (Books[_slno]);
     }
 
-    function closeBank() public onlyOwner {
-        //deprecated. Reset the code to default state
-        // selfdestruct(payable (owner))
-        //Allow owner to close account only if all other's  got their money 
-        // require(addrCount==0,"You can't close the bank when existing users are there");
-        
-        payable (owner).transfer(address(this).balance);
-        
+    function getBooklist() public view returns(uint [] memory) {
+        return booklist;
     }
 
-    //withdraw
-    function withdraw(uint amount) public  {
-       require(ledgerBalance[msg.sender] > amount,"Not enough balance");
-       ledgerBalance[msg.sender] = ledgerBalance[msg.sender] - (amount );
-       payable (msg.sender).transfer(amount);
-
-    }
-
-    function mybalance() public view returns (uint){
-        return ledgerBalance[msg.sender];
-    }
-    
-    //Function to Transfer ownership
-    function transferOwner(address newOwner) public onlyOwner {
-        owner = newOwner;
-    }
 }
