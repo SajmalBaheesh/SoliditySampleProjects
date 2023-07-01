11fba137b1338c32b4a7f7efb3a7305851df1f98
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-07-01 16:03:28 +0530

    Update BookStore.sol:use token

--- TokenExample/BookStore.sol
+++ TokenExample/BookStore.sol
@@ -9,19 +9,17 @@ contract BookStore is Token {
     struct Book {
         string title;
         uint8 price;
-        address owner;
         bool sold;
     }
 
-    uint[] public booklist;
+    uint[]  booklist;
 
     mapping(uint => Book) books;
 
     // fun setting book details
-    // set title price owner
     function setBookDetails(string memory _title, uint8 _price) public returns (uint)  {
         uint _slno = createToken();
-        books[_slno] = Book(_title, _price, msg.sender, false);
+        books[_slno] = Book(_title, _price, false);
         booklist.push(_slno);
         return (_slno);
     }
@@ -29,12 +27,12 @@ contract BookStore is Token {
     // buys - ownership tranfer
     // buyer provider ether > price
     function buyBook(uint _slno) public payable {
-        require(books[_slno].owner != msg.sender, 'Already owns it');
+        require(tokenOwner[_slno] != msg.sender, 'Already owns it');
 
         if ((msg.value / 1 ether) == books[_slno].price) {
-            books[_slno].owner = msg.sender;
+            changeTokenOwner(_slno,msg.sender);
         } else if ((msg.value / 1 ether) > books[_slno].price) {
-            books[_slno].owner = msg.sender;
+            changeTokenOwner(_slno,msg.sender);
             uint256 rm = msg.value - books[_slno].price * 1 ether;
 
             payable(msg.sender).transfer(rm);
@@ -43,7 +41,11 @@ contract BookStore is Token {
         }
     }
 
-    function getBookDetails(uint _slno) public view returns (Book memory) {
-        return (books[_slno]);
+    function getBookList() public view returns (uint[] memory) {
+        return booklist;
+    }
+
+    function getBookDetails(uint _slno) public view returns (Book memory,address) {
+        return (books[_slno],tokenOwner[_slno]);
     }
 }
