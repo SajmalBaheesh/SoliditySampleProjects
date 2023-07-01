499dd38e92fcc60edc1698d1ce4ff27d2f901ddd
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-07-01 14:39:30 +0530

    Update bankContract.sol:Added Transfer ownership function

--- bankContract.sol
+++ bankContract.sol
@@ -6,12 +6,15 @@ pragma solidity 0.8.19;
 
 contract Bank{
     mapping(address => uint) ledgerBalance;
+    mapping (uint=>address) addressIndex;
     address public owner; 
+    uint64 public addrCount;
     
     //Use a constructor - Triggered at the time of deployment
     constructor() {
         //Whoever deploys the contract will be the owner
         owner = msg.sender;
+        
     }
 
    //custom modifier, Functions with this modifier should pass the require condition 
@@ -26,6 +29,9 @@ contract Bank{
 
     //deposit
     function deposit() public payable {
+        if(ledgerBalance[msg.sender] ==0){
+            addressIndex[addrCount++] == msg.sender;
+        }
         ledgerBalance[msg.sender] = ledgerBalance[msg.sender] + msg.value;
         emit Depositevent(msg.sender,msg.value);
     }
@@ -44,8 +50,11 @@ contract Bank{
     function closeBank() public onlyOwner {
         //deprecated. Reset the code to default state
         // selfdestruct(payable (owner))
+        //Allow owner to close account only if all other's  got their money 
+        // require(addrCount==0,"You can't close the bank when existing users are there");
+        
         payable (owner).transfer(address(this).balance);
-        //TODO: Allow owner to close account only if all other's  got their money 
+        
     }
 
     //withdraw
@@ -59,5 +68,9 @@ contract Bank{
     function mybalance() public view returns (uint){
         return ledgerBalance[msg.sender];
     }
-    // TODO: Function to change ownership
+    
+    //Function to Transfer ownership
+    function transferOwner(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
 }
