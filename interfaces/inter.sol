46eb22cf28723d56288d3ce644b0d15185203693
Sajmal Baheesh <105344421+SajmalBaheesh@users.noreply.github.com>
2023-07-01 17:09:44 +0530

    Update inter.sol

--- interfaces/inter.sol
+++ interfaces/inter.sol
@@ -1,20 +1,35 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity 0.8.19;
 
-interface  Calc {
+interface Calc {
     function multi(uint a, uint b) external pure returns (uint);
     function add(uint a, uint b) external pure returns (uint);
 }
 
+// abstract contract A {
+//     function multi(uint a, uint b) external virtual pure returns (uint);
+//     function add(uint a, uint b) external pure returns (uint);
+// }
+
 contract Calculator is Calc {
-
-    function multi(uint a, uint b) external pure returns (uint){
-        return (a*b);
-    }
-    
-    function add(uint a, uint b) external pure returns (uint){
-        return (a+b);
+    function multi(uint a, uint b) public pure override returns (uint) {
+        return a * b;
     }
 
+   function add(uint a, uint b) external pure returns (uint) {
+       return a + b;
+   }
+}
 
+
+contract InteractCalc {
+    Calc obj;
+
+    constructor(address conAddr) {
+        obj = Calc(conAddr);
+    }
+
+    function getMul() public view returns (uint) {
+       return (obj.multi(4, 5));
+    }
 }
